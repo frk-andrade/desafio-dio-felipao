810159f3e16404be49bfa772b9daa3a9e586adfe
Franklin Andrade <100174287+frk-andrade@users.noreply.github.com>
2024-11-08 14:08:29 -0300

    Update index.js
    Espaço na concatenação

--- aulas/index.js
+++ aulas/index.js
@@ -20,4 +20,4 @@ if (nivelExperiecia <= 1000) {
     nivelHeroi = "Radiante"
 };
 
-console.log("O Herói de nome " + nomeHeroi + "está no nível de " + nivelHeroi );
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi );
